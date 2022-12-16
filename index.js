795be950e4b5b3bbb9b398793101ce5ae1da351e
Uvais Ahmad <uvss21@gmail.com>
2022-12-16 12:21:13 +0530

    Setup routes and Controller

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const db = require('./config/mongoose');
 //its convert form sending data to json data
 app.use(express.urlencoded());
-
+app.use('/',require('./router'));
 
 app.listen(port , function( err ){
     if(err){
---
+++ router/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('/api/v1/register');
+router.get('/api/v1/login');
+
+module.exports = router;
