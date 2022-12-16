bf06921f52da43ce26574fa5f17314da8fc43fd3
Uvais Ahmad <uvss21@gmail.com>
2022-12-16 12:09:10 +0530

    Connect with mongodb

---
+++ config/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/Auth-API');
+const db = mongoose.connection;
+
+db.on("error" , console.error.bind(console , "Error while connecting with the database "));
+db.once('open',function(){
+    console.log('Mongoose connected ');
+})
+
+module.exports = db;
