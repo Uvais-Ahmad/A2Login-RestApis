a8ae36d7349fe6fd3d461793d73fac836916f070
Uvais Ahmad <uvss21@gmail.com>
2023-01-25 13:00:06 +0530

    Setup home route for further details

--- router/index.js
+++ router/index.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
+app.get('/', (req , res )=>{
+    return res.status(200).json({
+        message : "Home For A2 LogIn Rest APIs",
+        Routes : {
+            UserRegister : 'https://a2-login-restapis.onrender.com/api/v1/register',
+            Login : 'https://a2-login-restapis.onrender.com/api/v1/login',
+            Profile : 'https://a2-login-restapis.onrender.com/api/v1/profile',
+            logout : 'https://a2-login-restapis.onrender.com/api/v1/logout'
+        },
+        README : "Please Go through the readme file before testing the restAPI",
+        READmeURL : 'https://github.com/Uvais-Ahmad/A2Login-RestApis/blob/master/README.MD'
+    })
+})
+
 router.use('/api/v1',require('./users'));
 
 module.exports = router;
